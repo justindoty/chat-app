b0930345d026a1b1d84cf7cd020f22e5b00eec08
Justin Doty <justindoty12@gmail.com>
2017-10-01 15:20:05 -0500

    loginvc

---
+++ chat-app/Controller/LoginVC.swift
@@ -0,0 +1,26 @@
+//
+//  LoginVC.swift
+//  chat-app
+//
+//  Created by Justin  Doty on 10/1/17.
+//  Copyright © 2017 Justin  Doty. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    @IBAction func closePressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+
+}
---
+++ chat-app/Utilities/Constants.swift
@@ -0,0 +1,14 @@
+//
+//  Constants.swift
+//  chat-app
+//
+//  Created by Justin  Doty on 10/1/17.
+//  Copyright © 2017 Justin  Doty. All rights reserved.
+//
+
+import Foundation
+
+
+//Segues
+
+let TO_LOGIN = "toLogin"
