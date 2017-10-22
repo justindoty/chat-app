9cdcbcd89a91083b3f1762fd0894e951938edae6
Justin Doty <justindoty12@gmail.com>
2017-10-22 16:00:05 -0500

    loggin user

--- chat-app/Utilities/Constants.swift
+++ chat-app/Utilities/Constants.swift
@@ -14,6 +14,7 @@ typealias CompletionHandler = (_ Success: Bool) -> ()
 
 let BASE_URL = "https://northman-chat.herokuapp.com/v1/"
 let URL_REGISTER = "\(BASE_URL)account/register"
+let URL_LOGIN = "\(BASE_URL)account/login"
 
 //Segues
 
@@ -30,3 +31,8 @@ let LOGGED_IN_KEY = "loggedIn"
 let USER_EMAIL = "userEmail"
 
 
+//Headers
+
+let HEADER = [
+    "Content-Type": "application/json; charset=utf-8"
+]
