d510012f3a34a6d67e10c74087ce36464de691be
Justin Doty <justindoty12@gmail.com>
2017-10-22 14:52:37 -0500

    register user

--- chat-app/Controller/CreateAccountVC.swift
+++ chat-app/Controller/CreateAccountVC.swift
@@ -16,10 +16,37 @@ class CreateAccountVC: UIViewController {
         
     }
 
+    //Outlets
+
+    @IBOutlet weak var usernameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var passwordTxt: UITextField!
+    @IBOutlet weak var userImg: UIImageView!
+    
+    //Functions
+    
+    @IBAction func pickAvatarPressed(_ sender: Any) {
+    }
+    
+    @IBAction func pickBgColorPressed(_ sender: Any) {
+    }
+    
+    @IBAction func createAccntPressed(_ sender: Any) {
+        
+        guard let email = emailTxt.text , emailTxt.text != "" else { return }
+        guard let password = passwordTxt.text , passwordTxt.text != "" else { return }
+
+        AuthService.instance.registerUser(email: email, password: password)
+            { (success) in
+                if success {
+                    print("registered user!")
+                }
+            }
+    }
+    
     @IBAction func closeBtnPressed(_ sender: Any) {
         performSegue(withIdentifier: UNWIND, sender: nil)
     }
-    
-    
+
     
 }
--- chat-app/Utilities/Constants.swift
+++ chat-app/Utilities/Constants.swift
@@ -8,6 +8,12 @@
 
 import Foundation
 
+typealias CompletionHandler = (_ Success: Bool) -> ()
+
+//URL Constants
+
+let BASE_URL = "https://northman-chat.herokuapp.com/v1/"
+let URL_REGISTER = "\(BASE_URL)account/register"
 
 //Segues
 
@@ -16,3 +22,11 @@ let TO_LOGIN = "toLogin"
 let TO_CREATE_ACCOUNT = "toCreateAccount"
 
 let UNWIND = "unwindToChannel"
+
+// USER DEFAULTS
+
+let TOKEN_KEY = "token"
+let LOGGED_IN_KEY = "loggedIn"
+let USER_EMAIL = "userEmail"
+
+
